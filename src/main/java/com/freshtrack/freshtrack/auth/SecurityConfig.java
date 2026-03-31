1fce342c11fd19be3ee935fe725835cfde3f3205
Aradhya Singh <aradhya07singh@gmail.com>
2026-03-31 23:13:53 +0530

    update security configuration and externalize database properties in application.properties

--- src/main/java/com/freshtrack/freshtrack/auth/SecurityConfig.java
+++ src/main/java/com/freshtrack/freshtrack/auth/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://freshtrack-theta.vercel.app"));
         configuration.setAllowedMethods(
             List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
